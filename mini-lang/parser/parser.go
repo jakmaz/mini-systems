1f943edcd770cc7a35d19bb47078209c2f3a77e2
Jakub Mazur <jakmaz.social@icloud.com>
2026-02-17 20:50:53 +0100

    feat: integer literal parsing

--- mini-lang/parser/parser.go
+++ mini-lang/parser/parser.go
@@ -5,6 +5,7 @@ import (
 	"minilang/ast"
 	"minilang/lexer"
 	"minilang/token"
+	"strconv"
 )
 
 const (
@@ -38,6 +39,7 @@ func New(l *lexer.Lexer) *Parser {
 	p := &Parser{l: l, errors: []string{}}
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 
 	// Read two tokens, so curToken and peekToken are both set
 	p.nextToken()
@@ -140,6 +142,18 @@ func (p *Parser) parseExpression(precedance int) ast.Expression {
 	}
 	leftExp := prefix()
 	return leftExp
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+	lit.Value = value
+	return lit
 }
 
 func (p *Parser) expectPeek(t token.TokenType) bool {
