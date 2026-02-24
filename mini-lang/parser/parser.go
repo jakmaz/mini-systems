e070401a75152f65e4cd71e2efb6e0a4bee1e619
Jakub Mazur <jakmaz.social@icloud.com>
2026-02-24 19:24:49 +0100

    feat: parentheses grouping

--- mini-lang/parser/parser.go
+++ mini-lang/parser/parser.go
@@ -55,6 +55,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 	p.registerPrefix(token.TRUE, p.parseBoolean)
 	p.registerPrefix(token.FALSE, p.parseBoolean)
+	p.registerPrefix(token.LPAREN, p.parseGroupExpression)
 
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
 	p.registerInfix(token.PLUS, p.parseInfixExpression)
@@ -233,6 +234,19 @@ func (p *Parser) parseBoolean() ast.Expression {
 	defer untrace(trace("parseBoolean"))
 
 	return &ast.Boolean{Token: p.curToken, Value: p.curTokenIs(token.TRUE)}
+}
+
+func (p *Parser) parseGroupExpression() ast.Expression {
+	defer untrace(trace("parseGroupExpression"))
+
+	p.nextToken()
+	exp := p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
+	return exp
 }
 
 func (p *Parser) expectPeek(t token.TokenType) bool {
