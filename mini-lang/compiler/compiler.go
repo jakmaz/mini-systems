9b82397317bcf0d24820c6316f760e73e664a7dc
Jakub Mazur <jakmaz.social@icloud.com>
2026-06-07 13:17:34 +0200

    feat: let statements and identifiers

--- mini-lang/compiler/compiler.go
+++ mini-lang/compiler/compiler.go
@@ -12,6 +12,7 @@ type Compiler struct {
 	constants           []object.Object
 	lastInstruction     EmmittedIntruction
 	previousInstruction EmmittedIntruction
+	symbolTable         *SymbolTable
 }
 
 func New() *Compiler {
@@ -20,7 +21,15 @@ func New() *Compiler {
 		constants:           []object.Object{},
 		lastInstruction:     EmmittedIntruction{},
 		previousInstruction: EmmittedIntruction{},
+		symbolTable:         NewSymbolTable(),
 	}
+}
+
+func NewWithState(s *SymbolTable, constants []object.Object) *Compiler {
+	compiler := New()
+	compiler.symbolTable = s
+	compiler.constants = constants
+	return compiler
 }
 
 func (c *Compiler) Compile(node ast.Node) error {
@@ -145,7 +154,21 @@ func (c *Compiler) Compile(node ast.Node) error {
 
 		afterAlternativePos := len(c.instructions)
 		c.changeOperand(jumpPos, afterAlternativePos)
+	case *ast.LetStatement:
+		err := c.Compile(node.Value)
+		if err != nil {
+			return err
+		}
 
+		symbol := c.symbolTable.Define(node.Name.Value)
+		c.emit(code.OpSetGlobal, symbol.Index)
+	case *ast.Identifier:
+		symbol, ok := c.symbolTable.Resolve(node.Value)
+		if !ok {
+			return fmt.Errorf("undefined variable %s", node.Value)
+		}
+
+		c.emit(code.OpGetGlobal, symbol.Index)
 	}
 	return nil
 }
