6c5ccbd93efaf8e7f0af41245cf241a72fe7b3f7
Jakub Mazur <jakmaz.social@icloud.com>
2026-06-08 14:06:25 +0200

    feat: strings

--- mini-lang/compiler/compiler.go
+++ mini-lang/compiler/compiler.go
@@ -96,6 +96,9 @@ func (c *Compiler) Compile(node ast.Node) error {
 	case *ast.IntegerLiteral:
 		integer := &object.Integer{Value: node.Value}
 		c.emit(code.OpConstant, c.addConstant(integer))
+	case *ast.StringLiteral:
+		str := &object.String{Value: node.Value}
+		c.emit(code.OpConstant, c.addConstant(str))
 	case *ast.Boolean:
 		if node.Value {
 			c.emit(code.OpTrue)
@@ -169,6 +172,7 @@ func (c *Compiler) Compile(node ast.Node) error {
 		}
 
 		c.emit(code.OpGetGlobal, symbol.Index)
+
 	}
 	return nil
 }
